ec75440ba38bc6eb179e7532495c5cd4ba2b48dc
MAX DE JONG <100854391@student.swin.edu.au>
2017-10-26 14:47:48 +1100

    ContactTableUpdate

--- EDSL/EDSL_Database/Database2/Contact.sql
+++ EDSL/EDSL_Database/Database2/Contact.sql
@@ -6,7 +6,11 @@
     [gender] NVARCHAR(50) NOT NULL, 
     [address1] NVARCHAR(50) NOT NULL, 
     [address2] NVARCHAR(50) NULL, 
-    [postcode] INT NULL, 
+    [postcode] INT NULL,
+	[state] NVARCHAR(7) NOT NULL,
+	[suburb] NVARCHAR(50) NOT NULL,
     [phone] NVARCHAR(50) NOT NULL, 
-    [email] NVARCHAR(50) NULL
+    [email] NVARCHAR(50) NULL,
+	[age] int NOT NULL,
+	[birthDate] date not null,
 )
