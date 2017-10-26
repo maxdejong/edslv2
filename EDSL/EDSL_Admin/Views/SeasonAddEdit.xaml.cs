94bba5017eb768888ca808089dc6a7dab42f2734
MAX DE JONG <100854391@student.swin.edu.au>
2017-10-26 16:46:08 +1100

    OMG

--- EDSL/EDSL_Admin/Views/SeasonAddEdit.xaml.cs
+++ EDSL/EDSL_Admin/Views/SeasonAddEdit.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace EDSL_Admin.Views
 {
@@ -23,6 +27,40 @@ namespace EDSL_Admin.Views
         public SeasonAddEdit()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Add calender to list
+            BreakDateList.Items.Add(new DatePicker());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            BreakDateList.Items.RemoveAt(BreakDateList.SelectedIndex);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //Add to database
+            private const string URL = "https://sub.domain.com/objects.json?api_key=123";
+            private const string DATA = @"{""object"":{""name"":""Name""}}";
+
+            HttpClient client = new HttpClient();
+
+
+
+
+
+            EDSLToolbox.Reff.Title.Content = "Seasons (season added)";
+            EDSLToolbox.Reff.Display.Content = new Season();
+        }
+
+        private void BreakDateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Removebtn.IsEnabled = true;
+            if (BreakDateList.SelectedItem == null)
+                Removebtn.IsEnabled = false;
         }
     }
 }
--- EDSL/EDSL_Database/Database2/Season.sql
+++ EDSL/EDSL_Database/Database2/Season.sql
@@ -3,5 +3,6 @@
 	[seasonID] NVARCHAR(50) NOT NULL PRIMARY KEY,
 	[leaguePID] NVARCHAR(50) NOT NULL,
     [seasonYear] INT NOT NULL, 
-    [startDate] DATE NOT NULL
+    [startDate] DATE NOT NULL,
+	[length] int not null,
 )
